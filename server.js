f0f943e1e025b0f0498a4acdad73c93dca300eb3
Jorgen1040 <26395020+Jorgen1040@users.noreply.github.com>
2021-12-15 19:19:40 +0100

    Remove 0-9 from regex

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ app.get("/join", (req, res) => {
 app.get("/:id", (req, res) => {
     let id = req.params.id;
     // Return error if code is invalid
-    if (id.match(/^[a-z0-9]{4}$/)) {
+    if (id.match(/^[a-z]{4}$/)) {
         return res.redirect("/?invalid=" + id);
     }
     if (games[id]) {
